b75b5f19abbbda6c5520a70b7477b4db616a27af
jeimi <jeimi@405a7194-22b4-4be1-997e-3b0534422c86>
2014-03-13 22:06:09 +0000

    new region site creation

---
+++ www/iseRegionalWebsites/_featureList.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+	<script type="text/javascript" src="js/tabs.js"></script>
+     
+<style type="text/css">
+/* <![CDATA[ */
+
+.sectionBody * {margin: 0; padding: 0;}
+
+.sectionBody {
+	float: right;
+	margin-top: 5px;
+	margin-right: 40px;
+	margin-bottom: 0px;
+	margin-left: 20px;
+} 
+.sectionBody .section.vertical a {
+	color: #17468E;
+}
+.sectionBody .section.vertical p {
+	padding-top: 7px;
+	padding-right: 7px;
+	padding-bottom: 7px;
+	padding-left: 7px;
+	font-size: 80%;
+}
+.sectionBody .section.vertical .box h1, h2, h3, h4, h5 {
+	font-weight: normal;
+	line-height: normal;
+	margin-top: 0;
+	margin-right: 0;
+	margin-bottom: 7px;
+	margin-left: 7px;
+}
+.sectionBody .section.vertical .box h1 {
+	font-size: 20px;
+}
+.sectionBody .section.vertical .box h2 {
+	font-size: 16px;
+	margin-left: 7px;
+}
+.sectionBody .section.vertical .box h3 {
+	font-size: 14px;
+	margin-left: 7px;
+}
+
+.sectionBody .section {
+	width: 600px;
+	background: #EFEFEF;
+	margin: 0 0 30px;
+}
+.sectionBody ul.tabs {
+	height: 40px;
+	line-height: 42px;
+	list-style: none;
+	border-bottom: 1px solid #DDD;
+	background: #FFF;
+	font-size: 90%;
+}
+.sectionBody .tabs li {
+	float: left;
+	display: inline;
+	color: #A7A9AC;
+	cursor: pointer;
+	background: #E6E7E8;
+	border: 1px solid #E4E4E4;
+	border-bottom: 1px solid #F9F9F9;
+	position: relative;
+	margin-top: 0;
+	margin-right: 1px;
+	margin-bottom: -1px;
+	margin-left: 0;
+	padding-top: 0;
+	padding-right: 13px;
+	padding-bottom: 1px;
+	padding-left: 20px;
+}
+.sectionBody .tabs li:hover,
+.sectionBody .vertical .tabs li:hover {
+	color: #17468E;
+	padding: 0 13px;
+	background: #7CA4C9;
+	border: 1px solid #17468E;
+}
+.sectionBody .tabs li.current {
+	color: #444;
+	background: #EFEFEF;
+	padding: 0 13px 2px;
+	border: 1px solid #D4D4D4;
+	border-bottom: 1px solid #EFEFEF;
+}
+.sectionBody .box {
+	display: none;
+	border: 1px solid #D4D4D4;
+  border-width: 0 1px 1px;
+	background: #17468E;
+	padding: 0 12px;
+}
+.sectionBody .box.visible {
+	display: block;
+	padding: 10px;
+}
+
+.sectionBody .section.vertical {
+	width: 500px;
+	border-left: 160px solid #FFF;
+}
+.sectionBody .vertical .tabs {
+	width: 160px;
+	float: left;
+	display: inline;
+	margin: 0 0 0 -160px;
+}
+.sectionBody .vertical .tabs li {
+	border: 1px solid #E4E4E4;
+	border-right: 1px solid #F9F9F9;
+	width: 132px;
+	height: 40px;
+	margin-top: 0;
+	margin-right: 0;
+	margin-bottom: 1px;
+	margin-left: 0;
+	padding-top: 8;
+	padding-right: 13px;
+	padding-bottom: 8;
+	padding-left: 13px;
+}
+.sectionBody .vertical .tabs li:hover {
+	width: 131px;
+}
+.sectionBody .vertical .tabs li.current {
+	width: 133px;
+	color: #fff;
+	background: #17468E;
+	border: 1px solid #17468E;
+  border-right: 1px solid #17468E;
+  margin-right: -1px;
+}
+.sectionBody .vertical .box {
+  border-width: 1px;
+}
+
+/* ]]> */
+.box .news {
+	font-size: 75%;
+	font-style: italic;
+	font-weight: bold;
+	color: #000;
+	height: 20px;
+	width: 95%;
+	padding-left: 20px;
+	left: 0px;
+	bottom: 0px;
+	background-color: #E5EAF5;
+	background-position: left bottom;
+	padding-top: 10px;
+	padding-bottom: 5px;
+}
+</style>
+<div class="sectionBody">
+<div class="section vertical">
+
+	<ul class="tabs">
+	  <li class="current">ISE NEWS</li>
+	  <li>FACEBOOK</li>
+	  <li>TWITTER</li>
+	  <li>BLOG</li>
+      <li>NEWSLETTER</li>
+      <li>WEBSTORE</li>
+	</ul>
+
+	<div class="box visible">
+    <div style="background-color: #FFF; padding: 15px;">
+		<p align="right"><small>ISE NEWS | March 5, 2014</small></p>
+          <h1><a href="press-page.cfm" target="_parent">International Student Exchange (ISE) is now Accepting Applications for Host Families</a></h1>
+<p>Babylon, NY &#8211; March 5th, 2014 – International Student Exchange is accepting applications for interested families to host students for their high school exchange program.  Students will be arriving in August 2014.</p>
+<p>ISE is the leading not-for-profit sponsor and has been finding homes for international exchange students in the United States for over 30 years.  They place students from over 40 countries for either semester or academic year programs. </p><p><a href="press-page.cfm">Full Story &gt;&gt;</a></p><br />
+<div class="news"><a href="press-page.cfm">MORE ISE NEWS >></a></div>
+    </div>
+	</div>
+
+	<div class="box">
+		<div style="background-color: #FFF; margin: 8px auto 8px auto;"><iframe
+src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fiseusa&amp;width=490&amp;colorscheme=light&amp;show_faces=false&amp;border_color&amp;stream=true&amp;header=true&amp;font=arial&amp;height=435" scrolling="yes" frameborder="0" style="border:none; overflow:hidden; width: 473px; height: 325px; background: white; float: left;" allowTransparency="true"></iframe><div class="clearfloat">&nbsp;</div>
+</div>
+
+	</div>
+
+	<div class="box">
+		<p> <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/ise_usa" data-widget-id="264412125424918530">Tweets by @ise_usa</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></p>
+<div class="clearfloat">&nbsp;</div>
+	</div>
+
+	<div class="box">
+<div style="background-color: #FFF; margin: 8px auto 8px auto; height: 350px; overflow: scroll; padding: 10px;">
+<h1>Most Recent News</h1>
+<div class="dotLine">&nbsp;</div>
+<p>&nbsp;</p>
+<?php 
+		//connection information 
+		@ $db = new mysqli('blog.iseusa.com','webAccess','Web@ccess$','blog');
+			if ($mysqli->connect_errno) {
+				echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+			}
+		//build the query
+		  $query='SELECT post_title, post_content, post_date, guid
+					FROM  wp_posts 
+					WHERE post_status = "publish"
+					ORDER BY post_Date DESC 
+					LIMIT 2';
+			
+		//run the query
+		$result = $db->query($query);
+		
+		
+		//display results
+		$num_results = $result->num_rows;
+		
+		//set up a loop
+        for ($i=0; $i <$num_results; $i++)	{
+		$row = $result->fetch_assoc();
+		
+		//dispaly the things on screen
+		echo "<h2>".($row['post_title'])."</h2>";
+		echo "<p>".($row['post_content'])."</p>";
+		echo "<p>".($row['post_date'])."</p>";
+		echo "<p>".($row['guid'])."</p>";
+		}
+		?>
+        <div class="news"><a href="http://blog.iseusa.com/">Go To BLOG >></a></div>
+            <div class="clearfloat">&nbsp;</div>
+        </div>
+        
+
+	</div>
+
+<div class="box">
+<div style="background-color: #FFF; padding: 10px; margin-top: 10px; margin-bottom: 10px;">
+<p align="right"><small>ISE NEWSLETTER | February 27, 2014</small></p>
+<h1>The Insider</h1>
+
+<h3>Next Month is Red Cross Month</strong>!</h3>
+<p>If your students haven’t done their Project HELP hours, next month there will be plenty of chances through the Red Cross. To find opportunities, direct them to our Project HELP Page: <a href="http://www.iseusa.com/student-exchange-project-help-redcross.cfm" class="blueLink">go to page</a></p>
+<div class="dotLine">&nbsp;</div>
+<br />
+<h3>Remember, we also send students to other countries too!</h3>
+<p>If you checked out our new website, you might have noticed the enhanced “study abroad" section. It’s a great chance for a US student to see another country and learn about their culture. We have full year programs, half year programs and summer programs. Check out all the countries you can visit: <a href="http://www.iseusa.com/study-abroad.cfm" class="blueLink">Study Abroad</a></p>
+<br />
+<div class="news"><a href="newsletter/host/February_host_newsletter.cfm" target="_blank">LATEST NEWSLETTER >></a></div>
+</div>
+<!-- .end box --></div>
+
+<!-- WEBSTORE -->
+<div class="box">
+<div style="background-color: #FFF; padding: 10px; margin-top: 10px; margin-bottom: 10px;">
+
+<h1><a href="webstore/index.php" target="_blank" class="blueLink" >Web Store</a></h1>
+<div class="dotLine">&nbsp;</div>
+ <div class="clearfloat" style="height: 10px;">&nbsp;</div>
+ <div class="asideImg" style="float:right; margin: 10px;"><a href="webstore/index.cfm" target="_blank"><img src="images/ISE_webstore.png" width="200" height="150" alt="ISE swag"  class="asideBrd" /></a></div>
+<p>The ISE Webstore has many great items for you to share with friends and family</p>
+     
+
+
+ <div class="clearfloat" style="height: 10px;">&nbsp;</div>
+<div class="news"><a href="http://www.iseusa.com/webstore/" target="_blank" >START SHOPPING >></a></div>
+<div class="clearfloat">&nbsp;</div></div>
+	</div>
+<!-- WEBSTORE -->    
+    
+</div><!-- .section -->
+</div>
---
+++ www/iseRegionalWebsites/_menu.php
@@ -0,0 +1,19 @@
+<div id="menuHolder">
+<div class="menu">
+<ul>
+<li><a href="http://www.iseusa.com" target="_self" >Home</a></li>
+<li style="padding-top:5px; color: #666;">|</li>
+<li><a href="host-a-student.cfm">Host a Student</a></li>
+<li style="padding-top:5px; color: #666;">|</li>
+<li><a href="meet-our-students.cfm">Meet our Students</a></li>
+<li style="padding-top:5px; color: #666;">|</li>
+<li><a href="join-our-team.php">Join Our Team </a></li>
+<li style="padding-top:5px; color: #666;">|</li>
+<li><a href="http://www.iseusa.com/student-project-help.cfm" target="_blank">Project H.E.L.P.</a></li>
+<li style="padding-top:5px; color: #666;">|</li>
+<li><a href="hosting-faq.cfm">FAQ</a></li>
+    <li style="padding-top:5px; color: #666;">|</li>
+<li><a href="contact-us.php" target="_blank">Contact Us</a></li>
+</ul>
+</div>
+</div>
