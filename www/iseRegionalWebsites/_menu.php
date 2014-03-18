a0d193ce979cd1d2ef40020d08bfdf33e58005c1
jeimi <jeimi@405a7194-22b4-4be1-997e-3b0534422c86>
2014-03-18 18:04:11 +0000

    Links working, fax removed, name added

--- www/iseRegionalWebsites/_menu.php
+++ www/iseRegionalWebsites/_menu.php
@@ -1,18 +1,18 @@
 <div id="menuHolder">
 <div class="menu">
 <ul>
-<li><a href="http://www.iseusa.com" target="_self" >Home</a></li>
-<li style="padding-top:5px; color: #666;">|</li>
-<li><a href="host-a-student.cfm">Host a Student</a></li>
-<li style="padding-top:5px; color: #666;">|</li>
-<li><a href="meet-our-students.cfm">Meet our Students</a></li>
-<li style="padding-top:5px; color: #666;">|</li>
+<li><a href="index.php" target="_self" >Home</a></li>
+<li style="padding-top:5px; color: #fff;">|</li>
+<li><a href="become-a-host-family.php">Host a Student</a></li>
+<li style="padding-top:5px; color: #fff;">|</li>
+<li><a href="meet-our-students.php">Meet our Students</a></li>
+<li style="padding-top:5px; color: #fff;">|</li>
 <li><a href="join-our-team.php">Join Our Team </a></li>
-<li style="padding-top:5px; color: #666;">|</li>
+<li style="padding-top:5px; color: #fff;">|</li>
 <li><a href="http://www.iseusa.com/student-project-help.cfm" target="_blank">Project H.E.L.P.</a></li>
-<li style="padding-top:5px; color: #666;">|</li>
-<li><a href="hosting-faq.cfm">FAQ</a></li>
-    <li style="padding-top:5px; color: #666;">|</li>
+<li style="padding-top:5px; color: #fff;">|</li>
+<li><a href="hosting-faq.php">FAQ</a></li>
+    <li style="padding-top:5px; color: #fff;">|</li>
 <li><a href="contact-us.php" target="_blank">Contact Us</a></li>
 </ul>
 </div>
