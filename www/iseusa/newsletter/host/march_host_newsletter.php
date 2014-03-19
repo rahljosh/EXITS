b8e5b2262c0cf85fa695c28fb1f30b6ee08a42ba
jeimi <jeimi@405a7194-22b4-4be1-997e-3b0534422c86>
2014-03-19 20:56:07 +0000

    new month

---
+++ www/iseusa/newsletter/host/march_host_newsletter.php
@@ -0,0 +1,135 @@
+<cfparam name="url.page" default="article1">
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>International Student Exchange - Newsletter</title>
+<style type="text/css">
+<!--
+-->
+</style>
+<link href="../css/theme.css" rel="stylesheet" type="text/css" />
+<link href="../css/BasicButtons.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="Hostheader"><!-- end .header -->
+    <div id="NLdate">March 2014</div>
+  </div>
+  <div class="sidebar1">
+  <div style="margin: 8px; background-color: #154690; color: #fff;">
+    <h1>Important Dates</h1>
+<p style="text-align: center; padding-bottom: 15px;"><strong>April Fool's Day<br />
+  </strong><em>April 1st</em>
+  <p style="text-align: center; padding-bottom: 15px;"><strong>Easter<br />
+  </strong><em>April 2oth</em></p>
+</div>
+  
+    <div style="margin: 8px; background-color: #A7A8AC; color: #000;">
+    <h1 style="color: #333333;">Contact Us</h1>
+<p style="text-align: center; padding-bottom: 15px; font-size: 90%; color: #333333;">
+<strong>Spencer Cordeau</strong><br />
+(631) 893-4540 x 115<br /><br />
+<em><strong>International Student Exchange</strong></em><br />
+119 Cooper Street<br />
+Babylon, NY 11702</p>
+</div> 
+    
+      <div style="margin: 8px; background-color: #154690; color: #fff;">
+    <h1>Quick Links</h1>
+    <table width="104" height="32" border="0" cellpadding="2" align="center">
+      <tr>
+        <td><a href="https://plus.google.com/b/106789919487817837927/106789919487817837927/posts" target="_blank"><img src="../images/g_22x22.png" width="22" height="22" /></a></td>
+        <td><a href="https://www.facebook.com/iseusa?ref=sgm" target="_blank"><img src="../images/ic_fbk_22.png" width="22" height="22" /></a></td>
+        <td><a href="https://twitter.com/ISE_USA" target="_blank"><img src="../images/ic_twit_22.png" width="22" height="22" /></a></td>
+        <td><a href="http://pinterest.com/iseusa/international-student-exchange-ise/" target="_blank"><img src="../images/pinterest-22.png" width="22" height="22" /></a></td>
+      </tr>
+    </table>
+    <p style="text-align: center; padding-bottom: 15px;">&nbsp;</p>
+</div>
+
+
+
+    <!-- end .sidebar1 --></div>
+  <div class="content">
+<!-- Article 1 -->
+
+<h2>Introduction:</h2>
+<p>Welcome to another edition of our new &ldquo;Host Newsletter&rdquo;.  Once again, feedback is always welcomed.</p>
+<div class="strongLine"></div>
+<div style="width: 150px; float: right; margin: 10px; overflow: hidden;"><a href="http://www.iseusa.com"><img src="../images/march-news.png" width="150"></a></div>
+<h1 style="margin-top: 10px;">The Latest....</h1>
+
+<h2>March is the time to sign up to host again!</h2>
+
+<p>This is a great time to sign up for to host next year.  There are still plenty of students looking for a host family.  Remember, the earlier you sign up, the better you get to know your student.</p>
+
+<div class="hrLine"></div>
+
+<h2>Red Cross Month</h2>
+
+<p>Now that the weather is warmer, there are many Project HELP opportunities.  We are still in the middle of Red Cross month.  Go out and tell your students to sign up today.  To find opportunities, direct them to our Project HELP Page: </p>
+<p align="center"><a href=" https://www.iseusa.com/student-project-help.cfm" class="basicOrangeButton" style="font-size: 80%;">Go to page</a></p>
+<!--<p align="center"><a href="https://www.iseusa.com/student-project-help.cfm" class="basicOrangeButton" style="font-size: 80%;">View Page</a></p>-->
+
+<div class="hrLine"></div>
+
+<h2>Capture the moments</h2>
+<p>Remember, send us pictures of what your students are up to.  Sports, clubs, vacations, spring break, family time . . . . the list goes on and we want to see it! Send  it all to us at either <a href="mailto:socialmedia@iseusa.com">socialmedia@iseusa.com</a> or message us on our facebook page <a href="http://www.facebook.com/iseusa">www.facebook.com/iseusa</a>.</p>
+
+<!-- END Article 1 -->
+
+<div class="strongLine"></div>
+<div id="div1"></div>
+<!-- END Article2 -->
+<div style="margin-bottom: 10px;"><img src="../images/michelleFamily.png" width="430" height="264" /></div>
+<h1>High-Five</h1>
+
+<h3>Giving a High-Five to the <strong>Juarbe Family!&nbsp; They are located out of Cleveland, OH.</strong></h3>
+<p><strong>Q1: What made you want to be a Host Family?</strong></p>
+<p class="grey"><em><strong>A: </strong>We wanted to be a host family to expose ourselves to a different culture and learn about other countries lifestyles, education and philosophies.</em></p>
+<div class="hrLine"></div>
+<p><strong>Q2: What’s the best part about being a Host Family?</strong></p>
+<p class="grey"><em><strong>A: </strong>The best part is now having a new "family" in Germany. We have grown so close to Niels entire family and feel that we are now related..</em></p>
+<div class="hrLine"></div>
+<p><strong>Q3: What do you usually do with your student(s) for fun?</strong></p>
+<p class="grey"><em><strong>A:</strong><em> We enjoy playing video games, playing cards, watching tv together and traveling. We are headed on spring break this week and plan to spend lots of time together walking on the beach and swimming in the ocean.</em></p>
+<div class="hrLine"></div>
+<p><strong>Q4: Any great stories you want to share?</strong></p>
+<p class="grey"><em><strong>A: </strong>need this</em></p>
+<div class="hrLine"></div>
+<p><strong>Q5: Any Advice for other host families?</strong></p>
+<p class="grey"><em><strong>A: </strong> Best advice. I didn't realize how much Niels family trusted me until he was here for a few days, unpacked and settled. I went in to his bedroom to say goodnight and saw his little stuffed animals and his small blanket. It hit me like a ton of bricks. I then knew that we were called to be a host family because we were open and willing to love and care for this young man, as if he were our own. Be open, be trustworthy and be respectful. If you are, you will get it back tenfold.</em></p>
+
+<!-- END Article 2 --> 
+
+<div class="strongLine"></div>
+<div id="div2"></div>
+<!-- Article 3 -->
+<div style="margin-bottom: 10px;"><img src="../images/facilitator_oct.png" width="430" height="264" /></div>
+<h1>Facilitator Square</h1>
+<p><strong>March 2014</strong></p>
+<p>As the end of the program nears, we&rsquo;d like to send a warm thank you to all our ISE host families.  You&rsquo;ve opened up your homes and hearts and given a young person an experience that will shape who they will become as an adult.  While we&rsquo;re sure that there have been the occasional bumps in the road, we hope that the experience for you and your family has been a very positive one.</p>
+<p>After a brutal winter, we’re starting to see signs of Spring.  With the warmer weather, students and their host families start getting even busier.  Students will begin playing spring sports, getting ready for prom season and keeping busy with their school work.  This is also the time students start to think about their return home.</p> 
+
+<p>Prom season and the end of the school year prompts a time for celebration.   While it is certainly a time to celebrate your student’s success, these celebrations can lead to temptations for students to engage in behaviors that are inappropriate (perhaps illegal) and a violation of ISE program rules.  Chief among these is drinking.</p>  
+
+<p>Now would be a good time to remind students that they are not allowed to consume alcohol or be in the presence of other students doing the same.  We’d hate to see a student have to return home because of a momentary lapse of judgment.</p>
+
+<p>With school soon coming to a close, it’s important to make sure your student maintains a focus on their studies.  For most students, their grades in the US affect their schooling back in their home countries.  Your student may just need a little encouragement to finish the year with good marks.</p>
+
+<!-- END Article 3 -->
+
+<div class="strongLine"></div>
+
+<div class="clearfloat">&nbsp;</div>
+<!-- end .content --></div>
+  <div class="footer">
+    <p style="text-align: center;">www.iseusa.com</p>
+<!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
