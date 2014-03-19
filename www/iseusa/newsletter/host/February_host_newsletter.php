8e857a2addcc13d2a064e37e7b0bbff99010bd8e
jeimi <jeimi@405a7194-22b4-4be1-997e-3b0534422c86>
2014-03-19 16:55:16 +0000

    new month

---
+++ www/iseusa/newsletter/host/February_host_newsletter.php
@@ -0,0 +1,166 @@
+<cfparam name="url.page" default="article1">
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>International Student Exchange - Newsletter</title>
+<style type="text/css">
+<!--
+-->
+</style>
+<link href="../css/theme.css" rel="stylesheet" type="text/css" />
+<link href="../css/BasicButtons.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="Hostheader"><!-- end .header -->
+    <div id="NLdate">February 2014</div>
+  </div>
+  <div class="sidebar1">
+  <div style="margin: 8px; background-color: #154690; color: #fff;">
+    <h1>Important Dates</h1>
+<p style="text-align: center; padding-bottom: 15px;"><strong>Daylight Savings<br />
+  </strong><em>March 9th</em>
+  <p style="text-align: center; padding-bottom: 15px;"><strong>St. Patrick's Day<br />
+  </strong><em>March 17th</em></p>
+</div>
+  
+    <div style="margin: 8px; background-color: #A7A8AC; color: #000;">
+    <h1 style="color: #333333;">Contact Us</h1>
+<p style="text-align: center; padding-bottom: 15px; font-size: 90%; color: #333333;">
+<strong>Spencer Cordeau</strong><br />
+(631) 893-4540 x 115<br /><br />
+<em><strong>International Student Exchange</strong></em><br />
+119 Cooper Street<br />
+Babylon, NY 11702</p>
+</div> 
+    
+      <div style="margin: 8px; background-color: #154690; color: #fff;">
+    <h1>Quick Links</h1>
+    <table width="104" height="32" border="0" cellpadding="2" align="center">
+      <tr>
+        <td><a href="https://plus.google.com/b/106789919487817837927/106789919487817837927/posts" target="_blank"><img src="../images/g_22x22.png" width="22" height="22" /></a></td>
+        <td><a href="https://www.facebook.com/iseusa?ref=sgm" target="_blank"><img src="../images/ic_fbk_22.png" width="22" height="22" /></a></td>
+        <td><a href="https://twitter.com/ISE_USA" target="_blank"><img src="../images/ic_twit_22.png" width="22" height="22" /></a></td>
+        <td><a href="http://pinterest.com/iseusa/international-student-exchange-ise/" target="_blank"><img src="../images/pinterest-22.png" width="22" height="22" /></a></td>
+      </tr>
+    </table>
+    <p style="text-align: center; padding-bottom: 15px;">&nbsp;</p>
+</div>
+
+
+
+    <!-- end .sidebar1 --></div>
+  <div class="content">
+<!-- Article 1 -->
+
+<h2>Introduction:</h2>
+<p>Welcome to another edition of our new &ldquo;Host Newsletter&rdquo;.  Once again, feedback is always welcomed.</p>
+<div class="strongLine"></div>
+<div style="width: 101px; float: right; margin: 10px; overflow: hidden;"><a href="http://www.iseusa.com"><img src="../../images/projectHelp/RedCross_button.jpg" width="101"></a></div>
+<h1 style="margin-top: 10px;">The Latest....</h1>
+
+<h2>Next Month is Red Cross Month</h2>
+
+<p>If your students haven’t done their Project HELP hours, next month there will be plenty of chances through the Red Cross.  To find opportunities, direct them to our Project HELP Page:</p>
+<p align="center"><a href="https://www.iseusa.com/student-project-help.cfm" class="basicOrangeButton" style="font-size: 80%;">View Page</a></p>
+
+<div class="hrLine"></div>
+
+<h2>Remember, we also send students to other countries too!</h2>
+
+<p>If you checked out our new website, you might have noticed the enhanced “study abroad" section.  It’s a great chance for a US student to see another country and learn about their culture.  We have full year programs, half year programs and summer programs.  Check out all the countries you can visit:
+
+</p>
+<p align="center"><a href="https://www.iseusa.com/study-abroad-outbound.cfm" class="basicOrangeButton" style="font-size: 80%;">Study Abroad</a></p>
+<!--<p align="center"><a href="https://www.iseusa.com/student-project-help.cfm" class="basicOrangeButton" style="font-size: 80%;">View Page</a></p>-->
+
+<div class="hrLine"></div>
+
+<h2>Capture the moments</h2>
+<p>Remember, send us pictures of what your students are up to.  Superbowl parties, Olympics, sports, clubs family time . . . . the list goes on and we want to see it! Send  it all to us at either <a href="mailto:socialmedia@iseusa.com">socialmedia@iseusa.com</a> or message us on our facebook page <a href="http://www.facebook.com/iseusa">www.facebook.com/iseusa</a>.</p>
+
+<!-- END Article 1 -->
+
+<div class="strongLine"></div>
+<div id="div1"></div>
+<!-- END Article2 -->
+<div style="margin-bottom: 10px;"><img src="../images/Feb-host-family.png" width="430" height="264" /></div>
+<h1>High-Five</h1>
+
+<h3>Giving a High-Five to the <strong>Juarbe Family!&nbsp; They are located out of Cleveland, OH.</strong></h3>
+<p><strong>Q1: What made you want to be a Host Family?</strong></p>
+<p class="grey"><em><strong>A: </strong>We became a host family because we tried a program with a student for a month (also from Spain) and loved it. Getting to learn about others cultures and sharing your home and caring for others is right for us. I'm a mommy to everyone and my husband is okay with that also. My grandchild also likes meeting new people.</em></p>
+<div class="hrLine"></div>
+<p><strong>Q2: What’s the best part about being a Host Family?</strong></p>
+<p class="grey"><em><strong>A: </strong>Knowing that you made a difference in a young person’s life while they leave an imprint forever in your heart.</em></p>
+<div class="hrLine"></div>
+<p><strong>Q3: What do you usually do with your student(s) for fun?</strong></p>
+<p class="grey"><em><strong>A:</strong><em> We like to visit new places.  We go to events such as carnivals or watch goofy and crazy movies.</em></p>
+<div class="hrLine"></div>
+<p><strong>Q4: Any great stories you want to share?</strong></p>
+<p class="grey"><em><strong>A: </strong>Truthfully, there are so many and we really get along well. I call Laura my Spanish daughter.</em></p>
+<div class="hrLine"></div>
+<p><strong>Q5: Any Advice for other host families?</strong></p>
+<p class="grey"><em><strong>A: </strong> I think future host families should really read the profiles well and find someone they know they have a few things in common with.  You don’t want a perfect fit because learning new things is fun. I'm sure if you have a loving spirit you will find that this is a life changing decision, and that you will be growing your family no doubt. I know have 2 Spanish daughters that I love dearly.</em></p></em>
+
+<!-- END Article 2 --> 
+
+<div class="strongLine"></div>
+<div id="div2"></div>
+<!-- Article 3 -->
+<div style="margin-bottom: 10px;"><img src="../images/facilitator_oct.png" width="430" height="264" /></div>
+<h1>Facilitator Square</h1>
+<p><strong>January 2014</strong></p>
+<p>Dear ISE Host Families:</p>
+<p>As we begin the slow warm-up from what has been a seemingly unending winter, we start looking ahead toward the conclusion of many students’ programs. They aren’t going home quite yet, but they should start preparing for their eventual return home. Things to consider at this time are return flights home and in the case of some students, educational record convalidations.</p>
+
+<h2>New Topics</h2>
+
+<h3>Return Flights Home</h3>
+
+<p>Students are expected to return home within five days of their last day of school. While some students would like to stay on longer, we find that we have fewer issues if students return home soon after school ends. Sometimes students will join natural family members who are traveling in the US, and that is acceptable. The student must realize, however,  that they are no longer to be considered on the program after the five day departure window. Exchange students should confirm their return flights with their agent or whoever booked their flight to assure that it still matches with their school departure dates. Host families and students should also coordinate travels plans to the airport for their departure. If you are unable to provide transportation to the airport for your student’s departure, please contact your ISE Area Representative.</p>
+
+<h3>Convalidations</h3>
+
+<p>If you’ve hosted a semester student from Brazil, Ecuador, Italy, Spain or Mexico, you may have already heard them mention something about a “convalidation”.  A convalidation is an official certification of a student’s transcript that they will need to present to their home school in order to get credit for the classes they took in the US.  It is the students’ responsibility to take care of their own convalidation process, but they may need your help in securing some of the documents since they will often have to return home before their transcript is available.  Usually, the host family just needs to forward the notarized transcript to the exchange student.  The convalidation process varies from country to country and links to the process are here:</p>
+
+<p style="margin-left:60px;"><strong>Brazil</strong><a href="../pdf/convalidation/Brazil-Convalidation.pdf" class="blueLink"> &#8211; &#171; view &#187;</a><br>
+  <strong>Ecuador </strong><a href="../pdf/convalidation/Ecuador-Convalidation.pdf" class="blueLink"> &#8211; &#171; view &#187;</a>
+  <br>
+  <strong>Italy</strong> <a href="../pdf/convalidation/Italy-Convalidation.pdf" class="blueLink"> &#8211; &#171; view &#187;</a>
+  <br>
+  <strong>Mexico</strong> <a href="../pdf/convalidation/Mexico-Convalidation.pdf" class="blueLink"> &#8211; &#171; view &#187;</a> 
+  <br>
+<strong>Spain</strong><a href="../pdf/convalidation/Spain-Convalidation.pdf" class="blueLink"> &#8211; &#171; view &#187;</a></p> 
+
+<p>(If your student requires a convalidation and is not from one of the countries listed above, please contact your ISE Area Representative)</p>
+
+<h2>Reminders</h2>
+
+<h3>Traveling with your Student</h3>
+
+<p>While many schools have already had their spring break, others will be coming up and families will often want to travel with their exchange student.  This is greatly encouraged and allows the student to experience more of the US.  Please notify your ISE Area Representative of your travel plans at least two-weeks prior to any domestic trip and one month prior to any cruise or international trip, including Canada and Mexico.</p>
+  
+<h3>Cruises and International Travel</h3>
+<p>If you are taking a cruise or traveling out of the country, students must have a signed travel authorization from ISE.  Each year we get panicked calls from families that are not able to board a cruise or are having trouble returning to the US because the student didn’t have an approved travel authorization.</p>
+<p>It’s up to the individual family, but it is OK for a host family to expect the exchange student to cover significant costs associated with a trip including airfare and their own hotel room and meals.</p>
+
+<h3>Visits from Natural Family</h3>
+<p>At this time of year, natural family members will be missing their sons and daughters and want to visit them in the US.  Natural family visits are not allowed during the ISE exchange program as we feel that it disrupts the student’s cultural acclimation and has the potential to cause problems.  Natural parents have all been notified in advance that these kinds of visits are not allowed. If you are asked by your student or their parents about a visit, please notify your ISE Area Representative and we’ll follow-up with the parent.</p> 
+<p>Natural parent visits are allowed at the end of the program, but it is up to each host family to determine whether this is acceptable and what the arrangements might be.  So for example, maybe you are OK with the natural family visiting, but don’t feel there is room in your home for one or more extra guests.  In that case, it is perfectly OK to suggest nearby hotels or B&Bs where the natural parents can stay.
+</p><!-- END Article 3 -->
+
+<div class="strongLine"></div>
+
+<div class="clearfloat">&nbsp;</div>
+<!-- end .content --></div>
+  <div class="footer">
+    <p style="text-align: center;">www.iseusa.com</p>
+<!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
