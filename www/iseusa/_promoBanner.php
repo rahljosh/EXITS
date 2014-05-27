3c15fb4da103eb8e34f8c5d4c86e99459e420b06
jeimi <jeimi@405a7194-22b4-4be1-997e-3b0534422c86>
2014-05-27 17:14:09 +0000

    New press release, May Newsletter and update Project HELP banner

--- www/iseusa/_promoBanner.php
+++ www/iseusa/_promoBanner.php
@@ -32,13 +32,13 @@ winslide.focus()
 //-->
 </script>
 
-<a href="javascript:gotoshow()"><img src="images/Promo/Promo_Banner_2.jpg" name="slide" width="620" height="150" alt="ISE News Banner"></a>
+<a href="javascript:gotoshow()"><img src="images/Promo/Promo_Banner_3.jpg" name="slide" width="620" height="150" alt="ISE News Banner"></a>
   <script>
 <!--
 
 //configure the paths of the images, plus corresponding target links
-slideshowimages("images/Promo/Promo_Banner_2.jpg","images/Promo/Promo_Banner_3.jpg", "images/Promo/Promo_Banner_1.jpg")
-slideshowlinks("https://www.facebook.com/iseusa?ref=sgm", "https://youtube.googleapis.com/v/-ScNJmjdlGo", "student-project-help.cfm")
+slideshowimages("images/Promo/Promo_Banner_3.jpg", "images/Promo/Promo_Banner_1.jpg")
+slideshowlinks("https://youtube.googleapis.com/v/-ScNJmjdlGo", "student-project-help.cfm")
 
 //configure the speed of the slideshow, in miliseconds
 var slideshowspeed=5000
