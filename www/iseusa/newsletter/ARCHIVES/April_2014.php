a447e542ca638935d7cc12409e6a02cb8b86b5f4
jeimi <jeimi@405a7194-22b4-4be1-997e-3b0534422c86>
2014-06-24 19:52:24 +0000

    June Rep Newsletter

---
+++ www/iseusa/newsletter/ARCHIVES/April_2014.php
@@ -0,0 +1,190 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>International Student Exchange - Newsletter</title>
+<style type="text/css">
+<!--
+-->
+</style>
+<link href="../css/theme.css" rel="stylesheet" type="text/css" />
+<link href="../css/BasicButtons.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header"><!-- end .header -->
+    <div id="NLdate">April 2014</div>
+  </div>
+  <div class="sidebar1">
+  <div style="margin: 8px; background-color: #154690; color: #fff;">
+    <h1>Important Dates</h1>
+<p style="text-align: center; padding-bottom: 15px;"><strong>Earth Day <br />
+  </strong><em>April 22nd</em><br /><br />
+ <strong>Mother's Day<br />
+  </strong><em>May 11th</em></p>
+</div>
+  
+  <ul class="nav">
+      <li><a href="http://www.iseusa.com/newsletter/" >HOME</a></li>
+    </ul>
+    
+      <div style="margin: 8px; background-color: #154690; color: #fff;">
+    <h1>Quick Links</h1>
+    <table width="104" height="32" border="0" cellpadding="2" align="center">
+      <tr>
+        <td><a href="https://plus.google.com/b/106789919487817837927/106789919487817837927/posts" target="_blank"><img src="../images/g_22x22.png" width="22" height="22" /></a></td>
+        <td><a href="https://www.facebook.com/iseusa?ref=sgm" target="_blank"><img src="../images/ic_fbk_22.png" width="22" height="22" /></a></td>
+        <td><a href="https://twitter.com/ISE_USA" target="_blank"><img src="../images/ic_twit_22.png" width="22" height="22" /></a></td>
+        <td><a href="http://pinterest.com/iseusa/international-student-exchange-ise/" target="_blank"><img src="../images/pinterest-22.png" width="22" height="22" /></a></td>
+      </tr>
+    </table>
+    <p style="text-align: center; padding-bottom: 15px;">&nbsp;</p>
+</div>
+
+    <div style="margin: 8px; background-color: #A7A8AC; color: #000;">
+    <h1 style="color: #333333;">Contact Us</h1>
+<p style="text-align: center; padding-bottom: 15px; font-size: 90%; color: #333333;">
+<strong>Spencer Cordeau</strong><br />
+(631) 893-4540 x 115<br /><br />
+<em><strong>International Student Exchange</strong></em><br />
+119 Cooper Street<br />
+Babylon, NY 11702</p>
+</div> 
+
+    <!-- end .sidebar1 --></div>
+  <div class="content">
+<!-- Article 1 -->
+ <div id="div1">
+    <div style="width: 150px; float: right; margin: 10px;"><a href="http://www.iseusa.com"><img src="../images/region-sites.jpg" width="150"></a></div>
+<h1>The Latest....</h1>
+
+<h2>The new websites are here</h2>
+
+<p>Each region has been given a new website.  Each website has a simple name - (region name).iseusa.org.<br />
+  <span class="blueLink">Example: west.ise.org (no more www)</span> </p>  
+<p>Your regional website will feature only students that are available in your area.  It will also provide one central location for your local families to go.</p>
+<p>Contact your regional manager with any questions.</p>
+
+<div class="hrLine"></div>
+
+<h2>Project HELP Hours</h2>
+
+<p>With the end of the year coming soon, it’s time to get the Project HELP hours in.  For those students who went above the 20 hour mark are eligible for a free tee-shirt.</p>
+<!---<p align="center"><a href="https://www.iseusa.com/student-project-help.cfm" class="basicOrangeButton" style="font-size: 80%;">Go to page</a></p>--->
+
+<div class="hrLine"></div>
+<!--
+<h2>Capture the Moments</h2>
+
+<p>Remember, send us pictures of what your students are up to.  Sports, clubs, vacations, spring break, family time . . . . the list goes on and we want to see it! Send it all at <a href="#">socialmedia@iseusa.com</a> or message us on our facebook page <a href="http://www.facebook.com/iseusa">www.facebook.com/iseusa</a>.</p>--->
+<!-- end DIV1 --></div>
+
+<div id="div2">
+
+<h1>What about Bob!</h1>
+
+<div style="width: 225px; float: right; margin: 10px;"><img src="../images/bob-news.png" width="225" height="150" alt="Happy Holidays from ISE" /></div>
+<h3>Likes and Leads</h3>
+<p>Our communications Department has been working very hard in developing new methods of help you garner leads and placements, particularly with regard to social media. With each region now having their own website and Facebook page, the amount of likes and leads has increased substantially and will continue to do so as we move forward.</p> 
+<p>The regions that are following up on their “likes” are finding new host families and in many cases signing on new reps. If they took the time to “like” what you are doing, it is certainly worth a few moments to follow up. A simple message, “If you like us why not become part of the ISE family and host a student or become an ISE representative.”</p>
+<p>The latest data from the Communications Department tells us that one in three of our prescreened host family leads become an ISE host family. That is a fact you cannot ignore.</p>
+<p>So please make it your business to follow up on your likes and leads....you will be glad you did!</p> 
+
+<p>Sincerely,</p>
+<h2 style="font-family: 'Yesteryear', cursive;">Bob Keegan</h2>
+<!-- end DIV2 --></div>
+    
+   
+<div id="div3">
+<div style="margin-bottom: 10px;"><img src="../images/compliance-names.png" width="430" height="264" /></div>
+
+<h1>Compliance Corner</h1>
+<p>With the end of the school year near, I would like to go over three main points to keep in mind:</p>
+
+<h3>1)	Host Family Orientations</h3>
+
+<p>We have been receiving host family orientations for families hosting 14/15 school year students.  Host families should not have an orientation until a few weeks prior to the student arrival.  This way the information provided during the orientation is fresh in their mind and paperwork provided will not be lost is misplaced. The orientation information is an intricate part of the hosting experience.</p>
+
+<h3>2)	School Acceptances</h3>
+
+<p>30 % of the school acceptances received by the Compliance Department are not compliant.  Missing information continues to be a problem. As stated by the Department of State during one of our audits, "If there is a question, there has to be an answer.  Please be more diligent in completing this document.  This will hold up a placement payment to the area representative.
+3) Reviewing Host Family Applications
+When reviewing a host family application, always repeat to yourself:<br /><br />
+ <em>"would I put my son or daughter in this home?"</em></p>  
+
+<p>If the answer is no, don't pass the application onto HQ for approval.  It is important to remember there is a natural parent and student waiting anxiously to review and see where the student will be residing for their program duration.  Tidy room photos are a vital part of the host family application; not to mention the professionalism of the application provided by ISE.</p>
+
+<!-- end DIV3 --></div>
+
+<div id="div4">
+<div style="margin-bottom: 10px;"><img src="../images/Facilitator-names.png" width="430" height="264" /></div>
+<h1>Facilitator Square</h1>
+<p>We’re on the homestretch and headed toward the finish line.</p> 
+<p>This can often be one of the best times of the year for you and your exchange student.  By this point, they are likely very comfortable with their English skill and have truly become part of your family.  It’s a great time for family trips, visits to relatives and lots of outdoor activities.  Take advantage of the remaining weeks.  Make sure your student also uses their short time remaining on the program most efficiently.  Keep them busy and involved in family activities. </p>
+
+<p>While this is one of the best moments of the program, it’s also a time that presents many temptations to students that can cause problems.  To help make sure your student finishes the program successfully, here are some pointers:</p>
+<p><strong>Keep them focused on the present.</strong> As students get closer to going home, some might start to think ahead to their return home.  This can cause a “lack of focus” on their responsibilities to their host family, schoolwork and activities.  Make sure your exchange student doesn’t start to dramatically increase their communication with their natural family and friends from their home country.  It’s OK for them to start to make some preparations.  You just need to gently remind them that they are still on the program and to continue to follow their progress in school.</p>
+<p><strong>Address any “acting up” issues immediately.</strong> Some student’s think they can take advantage of the fact that they are going home.  They might start acting out and being disobedient.  Some of this might just be typical teenage behavior.  If your student does suddenly becomes defiant or starts engaging in behavior that is causing problems in your home, you should address these behaviors immediately.  Make it known that they still need to follow host family rules until the day they depart.  If issues don’t get better quickly, make sure you notify your ISE area representative.</p>
+<p><strong>Be wary of temptations</strong>.  As we mentioned in last month’s newsletter, spring is also a time for celebrations, family gatherings, proms and going away parties.  Sometimes these parties can present temptations for students to drink or use drugs.  When your student is going to a party or event, please make sure there will be adult supervision at the event.  Please remind your student that they cannot drink on the program.  Each year we have the unfortunate task of sending a handful of students home weeks from program completion because of drinking incident</p>
+<p>If you haven’t already done so, please review out some of the previous month’s detail on Convalidations and Natural Family visits.</p>
+<h3>A/R update</h3>
+<p><strong>Update to payment information displayed in EXITS</strong></p>
+<p>Please note that there will be a small update to the payment information shown in EXITS.  This new detail should help make it easier for you to see which checks have been cut and which fees that check includes.</p>  
+<p><strong>Payment Screen - full detail</strong></p>
+<img src="../images/April-payment-fullscreen.png" width="420" alt=""/>
+<p>&nbsp;</p>
+<p><strong>Payment Screen - left side</strong></p>
+<img src="../images/April-payment-leftside.png" width="420" alt=""/>
+<p>Fees earned will now be grouped by date without regard to program season so that you can see both your supervision fees for the current season and placement fees for next season on one screen.  The fees and payment will be grouped together and the check will be highlighted in green.</p>
+<p><strong>Payment Screen - right side</strong></p>
+<img src="../images/April-payment-rightside.png" width="420" alt=""/>
+<ul>
+  <li>Checks (and any credits) will show in the <strong>Checks/Credits</strong> column and placement fees, bonuses and supervision fees will show in the <strong>Fees Earned</strong> column.</li>
+<li>When a check is sent out, the comments field will reflect the anticipated arrival time of the check.  Please do not call the office about a check you are expecting until that window of time has passed.</li>
+<li>A The Balance column is a running balance of all fees earned but not yet paid.</li></ul>
+<p>Note that fees are processed every day, but checks are typically run on Mondays and Thursdays.  Fees that have been processed, but not paid, will show with a date of “Pending” until the corresponding check is run.</p>
+<p><strong>Pending Payments example</strong></p>
+<img src="../images/April-pending-payments.png" width="420" alt=""/>
+<p>&nbsp;</p>
+
+<!-- end DIV4 --></div>
+
+<div id="div5">
+<h1>Employee Spotlight</h1>
+
+<h3><strong>Rose Hawkins </strong>&#8211; <strong>Regional Director for the North Central Region</strong></h3>
+<div style="margin: 10px; width: 225px; float: right;"><img src="../images/april-employee.png" width="225" alt=""/></div>
+<p><strong>Q: Tell everyone about yourself. What is your background, interests and hobbies.</strong></p>
+<p class="grey"><em> <strong>A:</strong> Before student exchange I was in Law Enforcement (Deputy).  My family is my world and I love doing things while traveling with my kids (Even though they tease me with me no mercy).  Anyone who really knows me knows Student Exchange and ISE are my passion.  I love to cook, entertain and feed people (Everyone knows my love of bacon).  I just started water aerobics again and love it.  The music I listen to the most is 60's and 70's, but our yearly girls weekend is centered around country music.</em></p>
+<div class="hrLine"></div>
+<p><strong>Q: What is your job function in ISE?</strong></p>
+<p class="grey"><em><strong>A: </strong>I am the Regional Director for the North Central Region.</em></p>
+<div class="hrLine"></div>
+<p><strong>Q: What is it about your work that is the most challenging/satisfying? </strong></p>
+<p class="grey"><em><strong>A: Challenging:</strong><br /> -	Thinking up new things to keep my reps motivated.<br /><br />
+<strong>Satisfying:</strong><br />
+-	When I get emails, phone calls, letters, text, stating a student or a family had a life changing experience. <br /> 
+-	A teen will go into a home a stranger, but leave being loved as a family member.<br /> 
+-	I love it when kids come back to visit. <br /> 
+-	When a student that struggles in the beginning finishes their program with flying colors!!</em></p>
+<div class="hrLine"></div>
+<p><strong>Q: Tell us something we don't know about you. </strong></p>
+<p class="grey"><em><strong>A: </strong>I sky dived on a dare, I love doing the polar plunge for Special Olympics and I love a challenge.  I am a cancer survivor!</em></p>
+<div class="hrLine"></div>
+<p><strong>Q: What new projects are you going to be working on this year?</strong></p>
+<p class="grey"><em><strong>A: </strong>We'll be doing more nontraditional booths along with the school fairs and expos.</em></p>
+<!-- end DIV5 --></div>
+
+<div class="strongLine"></div>
+
+<div class="clearfloat">&nbsp;</div>
+<!-- end .content --></div>
+  <div class="footer">
+    <p style="text-align: center;">www.iseusa.com</p>
+<!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
