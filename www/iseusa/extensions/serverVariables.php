533bff1e0c3d2038cc4b4d32432665b3437ab6be
jamesgriffiths <jamesgriffiths@405a7194-22b4-4be1-997e-3b0534422c86>
2014-07-14 14:42:32 +0000

    ISEUSA - Deleting directory to recommit.

--- www/iseusa/extensions/serverVariables.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-
-foreach($_SERVER as $key_name => $key_value) {
-
-print $key_name . " = " . $key_value . "<br>";
-
-}
-
-?>
--- www/iseusa/outboundInfoRequest.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'extensions/includes/_pageHeader.php'; 
-include_once('formTemplates/_outboundInfoRequest.php');
-
-?>
