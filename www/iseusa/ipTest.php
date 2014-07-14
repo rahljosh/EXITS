3249499538ede96a551316c23aa98549ff035870
jamesgriffiths <jamesgriffiths@405a7194-22b4-4be1-997e-3b0534422c86>
2014-07-14 14:42:32 +0000

    ISEUSA - Deleting directory to recommit.

--- www/iseusa/ipTest.php
+++
@@ -1,4 +0,0 @@
-<?php
-$response = http_get("http://api.ipinfodb.com/v3/ip-country/?key=0fc7fb53672eaf186d2c41db1c9b63224ef8f31e0270d8c351d2097794352bfb&ip=96.56.128.58", array("timeout"=>1), $info);
-print_r($info);
-?>
--- www/iseusa/testVar.php
+++
@@ -1,9 +0,0 @@
-<?php
-@include_once('extensions/includes/dumpVars.php');
-@include_once('extensions/includes/dBug.php');
-//new dBug($myVariable);
-
-new dBug($_SERVER);
-
-
-?>
